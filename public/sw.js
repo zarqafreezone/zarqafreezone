36f2f5729df259ec2a9a764e5fff7e56c14647ef
deploy <deploy@zarqafreezone.onrender.com>
2026-08-26 09:49:15 +0000

    v53: رفع رقم نسخة الكاش

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    - الملفات الثابتة (css/js/icons): stale-while-revalidate
    - الـAPI والرفعات: شبكة فقط (دائماً أحدث البيانات)
    ========================================================================= */
-const CACHE = "zfz-v52";
+const CACHE = "zfz-v53";
 const SHELL = [
   "/",
   "/index.html",
